fd20f9943255a7bbfd43ec8cd34f54e417977d75
kenta-suzuki <kenta_suzuki@albert2005.co.jp>
2022-04-27 17:23:16 +0900

    トークン削除

--- src/plugins/slacker.py
+++ src/plugins/slacker.py
@@ -2,10 +2,10 @@ from slacker import Slacker
 
 
 def get_slacker():
-    return Slacker("xoxb-2293861735-1238450237589-5fHFpkRHGFtY8T5iX0OduJtr")
+    return Slacker("xxx")
 
 
-slack = Slacker("xoxb-2293861735-1238450237589-5fHFpkRHGFtY8T5iX0OduJtr")
+slack = Slacker("xxx")
 response = slack.users.list()
 users = response.body["members"]
 print(users)
